fbca541705da71b9a94fa3f80fa0361bfd1aa330
Shane Holloway <shane.holloway@ieee.org>
2020-01-31 17:47:19 -0700

    Changed import details

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,28 @@ import rpi_resolve from '@rollup/plugin-node-resolve'
 import { terser as rpi_terser } from 'rollup-plugin-terser'
 import rpi_jsy from 'rollup-plugin-jsy-lite'
 
+import pkg from './package.json'
+const pkg_name = pkg.name.replace('-', '_')
+
 const configs = []
 export default configs
+
 const sourcemap = true
+const external = []
 
-const plugins = [ rpi_resolve(), rpi_jsy(), ]
-const plugins_min = [ ... plugins, rpi_terser({}), ]
+const plugins = [ rpi_resolve() ]
+const plugins_nodejs = [
+  rpi_jsy({defines: {PLAT_NODEJS: true}}),
+  ... plugins ]
+const plugins_web = [
+  rpi_jsy({defines: {PLAT_WEB: true}}),
+  ... plugins ]
+const plugins_min = null && [
+  ... plugins_web,
+  rpi_terser({}) ]
 
 
-add_jsy('index', {name: 'asn1_codec'})
+add_jsy('index', pkg_name)
 add_jsy('asn1_codec_plus')
 
 add_jsy('asn1_decode')
@@ -28,21 +41,28 @@ add_jsy('ecc/ecdsa/ecdsa_decode')
 //add_node_jsy('nodejs/ecdh')
 
 
-function add_jsy(srcname, {outname, name, exports, node_only}={}) {
-  if (!name) name = srcname.replace(/[_\/]/g, '_')
-  if (!outname) outname = srcname
-  if (!exports) exports = 'named'
+function add_jsy(src_name, module_name) {
+  if (!module_name) module_name = `${pkg_name}_${src_name}`
 
-  configs.push({
-    input: `code/${srcname}.jsy`, plugins,
-    output: [
-      { file: `esm/${outname}.mjs`, format: 'es', sourcemap },
-      { file: `cjs/${outname}.js`, format: 'cjs', exports, sourcemap },
-      !node_only && { file: `umd/${outname}.dbg.js`, format: 'umd', name, exports, sourcemap },
-  ].filter(Boolean)})
+  if (plugins_nodejs)
+    configs.push({
+      input: `code/${src_name}.jsy`,
+      plugins: plugins_nodejs, external,
+      output: [
+        { file: `cjs/${src_name}.js`, format: 'cjs', exports:'named', sourcemap },
+        { file: `esm/${src_name}.js`, format: 'es', sourcemap } ]})
 
-  !node_only && configs.push({
-    input: `code/${srcname}.jsy`, plugins: plugins_min,
-    output: { file: `umd/${outname}.min.js`, format: 'umd', name, exports }, })
+  if (plugins_web)
+    configs.push({
+      input: `code/${src_name}.jsy`,
+      plugins: plugins_web, external,
+      output: [
+        { file: `umd/${src_name}.js`, format: 'umd', name:module_name, exports:'named', sourcemap },
+        { file: `esm/web/${src_name}.js`, format: 'es', sourcemap } ]})
+
+  if (plugins_min)
+    configs.push({
+      input: `code/${src_name}.jsy`,
+      plugins: plugins_min, external,
+      output: { file: `umd/${src_name}.min.js`, format: 'umd', name:module_name, exports:'named', sourcemap }})
 }
-
