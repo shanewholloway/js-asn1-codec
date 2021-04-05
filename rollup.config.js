3cbe10d6bde079b4e6f1f927e93fda671857d0b0
Shane Holloway <shane.holloway@ieee.org>
2021-04-05 17:10:15 -0600

    Updated dependencies. Dropped CJS and UMD in favor of pure ESM module

--- rollup.config.js
+++ rollup.config.js
@@ -2,37 +2,28 @@ import rpi_resolve from '@rollup/plugin-node-resolve'
 import { terser as rpi_terser } from 'rollup-plugin-terser'
 import rpi_jsy from 'rollup-plugin-jsy-lite'
 
-import pkg from './package.json'
-const pkg_name = pkg.name.replace('-', '_')
-
-const configs = []
-export default configs
-
 const sourcemap = true
 
 const plugins = [ rpi_jsy(), rpi_resolve() ]
 const plugins_min = [ ... plugins, rpi_terser({}) ]
 
 
-add_jsy('index', {module_name: pkg_name})
-add_jsy('asn1_decode')
-add_jsy('asn1_encode')
+export default [
+  ... add_jsy('index'),
+  ... add_jsy('asn1_decode'),
+  ... add_jsy('asn1_encode'),
+]
 
 
-function add_jsy(src_name, opt={}) {
-  let module_name = opt.module_name || `${pkg_name}_${src_name}`
-
-  configs.push({
+function * add_jsy(src_name, opt={}) {
+  yield ({
     input: `code/${src_name}.jsy`,
     plugins,
-    output: [
-      { file: `esm/${src_name}.mjs`, format: 'es', sourcemap },
-      { file: `cjs/${src_name}.cjs`, format: 'cjs', exports:'named', sourcemap },
-      { file: `umd/${src_name}.js`, format: 'umd', name:module_name, exports:'named', sourcemap }, ]})
+    output: { file: `esm/${src_name}.mjs`, format: 'es', sourcemap }})
 
   if (plugins_min)
-    configs.push({
+    yield ({
       input: `code/${src_name}.jsy`,
       plugins: plugins_min,
-      output: { file: `umd/${src_name}.min.js`, format: 'umd', name:module_name, exports:'named', sourcemap }})
+      output: { file: `esm/${src_name}.min.mjs`, format: 'es', sourcemap }})
 }
